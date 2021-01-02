a639b96ece662fb68374aba0055f9534186e4cf0
ikhsanmoh <icanmohamad13@gmail.com>
2021-01-02 11:04:09 +0700

    Added Botom Navigation and its dependencies(Material Design) on modul Beranda

--- app/src/main/java/project/roll/BerandaActivity.java
+++ app/src/main/java/project/roll/BerandaActivity.java
@@ -1,10 +1,16 @@
 package project.roll;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.ArrayList;
 
@@ -14,6 +20,25 @@ import project.roll.model.Photographer;
 public class BerandaActivity extends AppCompatActivity {
 
   private RecyclerView photographerRecView;
+
+  private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+          new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+              Toast toast = null;
+
+              switch (item.getItemId()){
+                case R.id.nav_home:
+                  toast = Toast.makeText(getApplicationContext(),"Home Selected",Toast.LENGTH_SHORT);
+                  break;
+                case R.id.nav_categories:
+                  toast = Toast.makeText(getApplicationContext(),"Categories Selected",Toast.LENGTH_SHORT);
+                  break;
+              }
+              toast.show();
+              return true;
+            }
+          };
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -44,5 +69,8 @@ public class BerandaActivity extends AppCompatActivity {
 
     photographerRecView.setAdapter(adapter);
     photographerRecView.setLayoutManager(new GridLayoutManager(this, 2));
+
+    BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+    bottomNav.setOnNavigationItemSelectedListener(navListener);
   }
 }
